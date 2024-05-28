76a5e17f444d8353c332ae3ca00c8b084f043448
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2024-05-28 15:14:32 +0530

    Update run.py

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ def make_shell_context():
     return {'db': db, 'User': User, 'Website': Website}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8009)
